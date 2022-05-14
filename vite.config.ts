f868e467ed18f773144d5587d79352c3461a2aa0
seulgiyoon <sgyoon.info@gmail.com>
2022-05-14 22:02:50 +0900

    프로젝트 절대경로 import 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      styles: path.resolve(__dirname, './src/styles'),
+    },
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
