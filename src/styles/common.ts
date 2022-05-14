da56c8a73108822f89b082bc914d2093929073b8
seulgiyoon <sgyoon.info@gmail.com>
2022-05-14 22:39:27 +0900

    기본 html 마크업 작성

---
+++ src/styles/common.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react'
+
+// https://www.a11yproject.com/posts/how-to-hide-content/
+// 접근성을 보장하며 화면에서 요소를 보이지 않도록 설정하는 스타일
+export const visuallyHiddenStyle = css`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`
